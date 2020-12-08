c85cba8cfa9d49b7e35c26eb13796c3a2e8420a0
Samuel Simões <samuelsimoes31@gmail.com>
2020-12-08 10:40:47 -0300

    chaging print

--- DigitalInnovationOne/Program.cs
+++ DigitalInnovationOne/Program.cs
@@ -2,11 +2,14 @@
 
 namespace DigitalInnovationOne
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      for (int i = 0; i < 5; i++)
+      {
+        Console.WriteLine($"Hello World! [{i}]");
+      }
     }
+  }
 }
